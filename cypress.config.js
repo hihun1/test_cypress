406fbcd8fa55f06265dc192f514f8534c7553613
hihun1 <hung3002441@gmail.com>
2025-06-15 15:11:52 +0700

    Add Cypress Mochawesome HTML report

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,22 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'Cypress Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+      reportDir: 'cypress/reports/html',
+      overwrite: false,
+      html: true,
+      json: true
+    }
   },
   env:
   {
